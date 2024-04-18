0747a776e7253329bc852c22249929a5f5905193
Tebogo Ndaba <dennistebogo@gmail.com>
2024-04-18 14:56:43 +0200

    Initial commit

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,7 +4,6 @@ const request = util.promisify(require('request'));
 const filmID = process.argv[2];
 
 async function starwarsCharacters(filmId) {
-  try {
     const filmResponse = await request(`https://swapi-api.alx-tools.com/api/films/${filmId}`);
     const { characters } = JSON.parse(filmResponse.body);
 
@@ -13,9 +12,6 @@ async function starwarsCharacters(filmId) {
       const { name } = JSON.parse(characterResponse.body);
       console.log(name);
     }
-  } catch (error) {
-    console.error('Error:', error);
-  }
 }
 
 starwarsCharacters(filmID);
